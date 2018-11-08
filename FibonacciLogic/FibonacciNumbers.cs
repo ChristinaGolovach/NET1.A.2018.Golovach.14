51950fb3e4de954155d7a089d2e82f48175dc4fa
Christina <christina.golovach@gmail.com>
2018-11-08 20:25:46 +0300

    Add Logic

---
+++ FibonacciLogic/FibonacciNumbers.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace FibonacciLogic
+{
+    public static class FibonacciNumbers
+    {
+
+        public static ulong[] GenerateSequence(int count)
+        {
+            if (count <= 0)
+            {
+                throw new ArgumentException($"The {nameof(count)} must be more than one.");
+            }
+
+            if (count == 1)
+            {
+                return new ulong[] { 1 };
+            }
+
+            ulong[] numbers = new ulong[count];
+
+            numbers[0] = 1;
+            numbers[1] = 1;
+            int i = 2;
+
+            while(i < count)
+            {
+                numbers[i] = numbers[i - 1] + numbers[i - 2];
+                i++;
+            }
+
+            return numbers;
+        }
+    }
+}
