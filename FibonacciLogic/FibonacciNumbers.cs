15c1dc5b2bb7409ed5998d1bcf183fa681acae73
Christina <christina.golovach@gmail.com>
2018-11-09 18:46:11 +0300

    Add comments

--- FibonacciLogic/FibonacciNumbers.cs
+++ FibonacciLogic/FibonacciNumbers.cs
@@ -3,9 +3,23 @@ using System.Numerics;
 
 namespace FibonacciLogic
 {
+    /// <summary>
+    /// Class for the generation of Fibonacci numbers.
+    /// </summary>
     public static class FibonacciNumbers
     {
-
+        /// <summary>
+        /// Generate the sequence of Fibonacci numbers.
+        /// </summary>
+        /// <param name="count">
+        /// The length of result sequence.
+        /// </param>
+        /// <returns>
+        /// Array of Fibonacci numbers.
+        /// </returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when the value of <paramref name="count"/> less then one.
+        /// </exception>
         public static BigInteger[] GenerateSequence(int count)
         {
             if (count <= 0)
@@ -24,7 +38,7 @@ namespace FibonacciLogic
             numbers[1] = 1;
             int i = 2;
 
-            while(i < count)
+            while (i < count)
             {
                 numbers[i] = numbers[i - 1] + numbers[i - 2];
                 i++;
